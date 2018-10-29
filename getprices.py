b09f70f98174402da8ddb33dee517d015328e424
Sally Galuska <sgaluska@email.arizona.edu>
2018-10-28 18:15:15 -0700

    Program now reads CSVs

--- getprices.py
+++ getprices.py
@@ -5,6 +5,13 @@ import csv
 import sys
 
 if len(sys.argv)<2:
-    print "usage: python3 getprices.py input.csv"
+    print ("usage: python3 getprices.py input.csv")
 
-#with open(
+cardlist=[]
+
+with open(sys.argv[1]) as csvfile:
+    cardreader=csv.reader(csvfile, delimiter=",")
+    for row in cardreader:
+        cardlist.append(row[0])
+
+
